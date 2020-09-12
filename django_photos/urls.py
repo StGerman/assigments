85c24f472591d83103865054bda5fa55061dd781
Stanislav German <stas.german@gmail.com>
2020-09-12 20:29:58 +0300

    fix code style with black

--- django_photos/urls.py
+++ django_photos/urls.py
@@ -20,11 +20,11 @@ from django.contrib import admin
 from django.urls import path
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
+router.register(r"users", views.UserViewSet)
+router.register(r"groups", views.GroupViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path("", include(router.urls)),
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
