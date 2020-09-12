12aaf503e830fb79c80d60110a63e56357d3b6bf
Stanislav German <stas.german@gmail.com>
2020-09-13 01:32:15 +0300

    add requirements and dotenv

--- django_photos/api_v1/tests.py
+++ django_photos/api_v1/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+
+class Registration(TestCase):
+
+    client = Client()
+
+    def test_my_view(self):
+        data = {
+            "username": "test",
+            "email": "test@test.test",
+            "password": "password",
+            "password_confirmation": "password",
+        }
+        response = self.client.post("/register", data)
+        print(response)
+        assert response.status_code == 301
