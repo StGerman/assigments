d7355319ff58605238c1e53b0421bcd13ea5b181
Stanislav German <stas.german@gmail.com>
2020-09-12 17:16:49 +0300

    add users

--- django_photos/urls.py
+++ django_photos/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django_photos.api_v1 import views
+from rest_framework import routers
+from django.urls import include, path
 from django.contrib import admin
 from django.urls import path
 
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
 urlpatterns = [
+    path('', include(router.urls)),
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
