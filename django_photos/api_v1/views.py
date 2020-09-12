c08071d2e96ab6495d6a96c8b76780ff2d77d0fd
Stanislav German <stas.german@gmail.com>
2020-09-12 20:29:58 +0300

    fix code style with black

--- django_photos/api_v1/views.py
+++ django_photos/api_v1/views.py
@@ -30,7 +30,8 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    queryset = User.objects.all().order_by('-date_joined')
+
+    queryset = User.objects.all().order_by("-date_joined")
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
@@ -39,10 +40,7 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
     """
+
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
-
-
-"""
-"""
