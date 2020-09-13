85e9f00254113ab47365de1954efe791cdd71f16
Stanislav German <stas.german@gmail.com>
2020-09-13 17:17:54 +0300

    make migrations

--- django_photos/photos/models.py
+++ django_photos/photos/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Photo(models.Model):
     name = models.CharField(max_length=100)
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="todolist", null=True
+        User, on_delete=models.CASCADE, related_name="photo", null=True
     )
     image = models.ImageField()
 
@@ -16,7 +16,7 @@ class Photo(models.Model):
 class Comment(models.Model):
     photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="todolist", null=True
+        User, on_delete=models.CASCADE, related_name="comment", null=True
     )
     text = models.CharField(max_length=300)
 
